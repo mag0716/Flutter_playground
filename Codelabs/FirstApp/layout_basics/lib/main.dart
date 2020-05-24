56cfff107ba1e2bfea15e2bbeb6e465a832e45a1
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-05-24 22:25:32 +0900

    Part2

--- Codelabs/FirstApp/layout_basics/lib/main.dart
+++ Codelabs/FirstApp/layout_basics/lib/main.dart
@@ -20,19 +20,33 @@ class MyApp extends StatelessWidget {
 class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Icon(Icons.account_circle, size: 50),
-      ),
-      Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Flutter McFlutter',
-                style: Theme.of(context).textTheme.headline),
-            Text('Experienced Developer')
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.account_circle, size: 50),
+            ),
+            Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Flutter McFlutter',
+                      style: Theme.of(context).textTheme.headline),
+                  Text('Experienced Developer')
+                ]),
           ]),
-    ]);
+          SizedBox(height: 8),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text('123 Main Street.'),
+            Text('(415) 555-0198'),
+          ]),
+          SizedBox(height: 16),
+          Row(
+            children: [],
+          ),
+        ]);
   }
 }
