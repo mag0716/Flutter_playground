fc6f8bdcb7454a6212cd62cc4270df3276c989b2
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-05-31 21:27:43 +0900

    ローカライズ用のクラスを作成

---
+++ samples/I18nSample/i18n_sample/lib/i18n/i18n.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:i18nsample/i18n/messages_all.dart';
+import 'package:intl/intl.dart';
+
+class I18n {
+  I18n(this.localeName);
+
+  static Future<I18n> load(Locale locale) {
+    final String name =
+        locale.countryCode.isEmpty ? locale.languageCode : locale.toString();
+    final String localeName = Intl.canonicalizedLocale(name);
+    return initializeMessages(localeName).then((_) {
+      return I18n(localeName);
+    });
+  }
+
+  static I18n of(BuildContext context) {
+    return Localizations.of<I18n>(context, I18n);
+  }
+
+  final String localeName;
+}
