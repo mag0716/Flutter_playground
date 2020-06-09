1f9d3f4ce012067ed06e15ea1986f1d672eaf143
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-06-09 22:12:42 +0900

    flutter pub run build_runner build で生成

---
+++ samples/JsonParserSample/json_parser/lib/data_by_json_serializable.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'data_by_json_serializable.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DataByJsonSerializable _$DataByJsonSerializableFromJson(
+    Map<String, dynamic> json) {
+  return DataByJsonSerializable(
+    id: json['id'] as int,
+    title: json['title'] as String,
+  );
+}
+
+Map<String, dynamic> _$DataByJsonSerializableToJson(
+        DataByJsonSerializable instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+    };
