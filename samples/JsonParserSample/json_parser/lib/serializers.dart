a2ebda0942be7d0d322a534ecf97f22dfe228e05
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-06-10 22:31:25 +0900

    デシリアライズに失敗するので StandardJsonPlugin を add

--- samples/JsonParserSample/json_parser/lib/serializers.dart
+++ samples/JsonParserSample/json_parser/lib/serializers.dart
@@ -1,9 +1,11 @@
+library serializers;
+
 import 'package:built_value/serializer.dart';
+import 'package:built_value/standard_json_plugin.dart';
 import 'package:jsonparser/data_by_built_value.dart';
 
 part 'serializers.g.dart';
 
-@SerializersFor([
-  DataByBuiltValue
-])
-final Serializers serializers = _$serializers;
+@SerializersFor([DataByBuiltValue])
+final Serializers serializers =
+    (_$serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
