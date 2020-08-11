4a3e81dbb434508fa98db30bf8f509a4bbf4006c
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-08-11 21:37:28 +0900

    Customising some assets based on flavor 実装

--- samples/FlavorsSample/flavors_sample/lib/home_page.dart
+++ samples/FlavorsSample/flavors_sample/lib/home_page.dart
@@ -18,11 +18,13 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(config.appDisplayName),
       ),
-      body: _buildBody(config.appDisplayName, config.stringResource),
+      body: _buildBody(
+          config.appDisplayName, config.stringResource, config.appInternalId),
     );
   }
 
-  Widget _buildBody(String appName, StringResource stringResource) {
+  Widget _buildBody(
+      String appName, StringResource stringResource, int appInternalId) {
     return Container(
         margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
         child: Column(
@@ -30,8 +32,9 @@ class _HomePageState extends State<HomePage> {
             Text(appName),
             Text(DATE + getDateForDisplay()),
             Text(stringResource.APP_DESCRIPTION),
-            Image.asset("assets/dancing.png", width: 50.0, height: 50.0),
-            Image.asset("assets/1.png", width: 50.0, height: 50.0),
+            Image.asset('assets/dancing.png', width: 50.0, height: 50.0),
+            Image.asset('assets/' + appInternalId.toString() + '/icon.png',
+                width: 50.0, height: 50.0),
           ],
         ));
   }
